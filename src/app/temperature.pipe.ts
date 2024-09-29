e3516a49dc71a047a420fc377746687d00f59461
krishnachaithanya4716 <krishnachaithanya4716@gmail.com>
2024-09-29 12:33:08 +0530

    pushing final code to main

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,51 @@
+import { input, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true
+})
+export class TemperaturePipe implements PipeTransform {
+
+  transform(value: string | number | null,inputType:'cel'|'fah',outputType?:'cel'|'fah'):string {
+
+    let val:number;
+
+    if (value ==null || value == undefined){
+      return "invalid temprature"
+    }
+
+    if(typeof value=='string'){
+      val = parseFloat(value);
+    }
+    else{
+      val = value;
+    }
+    let outputTemp:number;
+
+    if (inputType==='cel' && outputType=='fah'){
+
+      outputTemp = val * (9/5) + 32;
+    }
+    else if (inputType==='fah' && outputType==='cel'){
+      outputTemp = (val-32)*5/9;
+    }
+    else  {
+      outputTemp = val
+    }
+
+
+    let symbol: '°F'| '°C'
+
+   if (!outputType){
+    symbol = inputType === 'cel' ? '°C' : '°F';
+   }
+   else {
+    symbol = outputType === 'cel' ? '°C' : '°F';
+   }
+
+    return outputTemp + symbol;
+
+    
+  }
+
+}
